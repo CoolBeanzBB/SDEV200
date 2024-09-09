f69b771b0c323bfdd429ae681309242d8eb2fb9b
CoolBeanzBB <150744657+CoolBeanzBB@users.noreply.github.com>
2024-09-08 20:37:34 -0400

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,37 @@
+public class Circle extends GeometricObject {
+    private double radius;
+
+    public Circle() {
+        radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    public String toString() {
+        return "Circle: radius = " + radius + ", " + super.toString();
+    }
+}
